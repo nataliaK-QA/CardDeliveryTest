f803741615d68fd19d7ab79ade40ee070aebbf47
Natalia Kashcheeva <n.kashcheeva96@gmail.com>
2025-04-17 12:25:52 +0300

    Fix test commit

--- src/test/java/ru/netology/selenide/CardDeliveryTest.java
+++ src/test/java/ru/netology/selenide/CardDeliveryTest.java
@@ -20,12 +20,12 @@ public class CardDeliveryTest {
     @Test
     public void shouldBeSuccessfullyCompleted() {
         open("http://localhost:9999");
-        $("[data-test-id='city'] input").setValue("Рязань");
+        $("[data-test-id='city'] input").setValue("Омск");
         String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         $("[data-test-id='date'] input").setValue(planningDate);
-        $("[data-test-id='name'] input").setValue("Иванов-Иваныч Иван");
-        $("[data-test-id='phone'] input").setValue("+78005553535");
+        $("[data-test-id='name'] input").setValue("Петр Петрович");
+        $("[data-test-id='phone'] input").setValue("+77777777777");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
